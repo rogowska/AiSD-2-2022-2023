0f15f6ae91655e31be97a5cd2c0d64051899d7a4
rogowska <rogowska.oliwia@gmail.com>
2023-03-07 11:53:24 +0100

    refining main file

--- priority_queue/test.cpp
+++ priority_queue/test.cpp
@@ -1,36 +1,45 @@
 #include "BinaryHeap.h"
 
-int main(){
-BinaryHeap<int> heap(10);
-std::cout << heap.IsEmpty() << std::endl;
+void test(BinaryHeap<int> heap){
+std::cout <<"IsEmpty: "<< heap.IsEmpty() << std::endl;
 heap.Enqueue(5);
-std::cout << heap.IsEmpty() << std::endl;
+std::cout <<"IsEmpty: "<< heap.IsEmpty() << std::endl;
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(6);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(8);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(3);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(9);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(2);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.Enqueue(1);
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
+std::cout << "Minimum dequeued: ";
 heap.DequeueMin();
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
+std::cout << "Minimum dequeued: ";
 heap.DequeueMin();
 heap.Print();
-std::cout << heap.FindMin() << std::endl;
+std::cout <<"Minimum: "<< heap.FindMin() << std::endl;
 heap.MakeNull();
-std::cout << heap.IsEmpty() << std::endl;
+std::cout <<"IsEmpty: "<< heap.IsEmpty() << std::endl;
+}
+
+int main(){
+BinaryHeap<int> heap(10);
+BinaryHeap<int> heap2(2);
+test(heap);
+std::cout<<std::endl;
+test(heap2);
 }
